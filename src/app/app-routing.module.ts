8110fe2a129051a2ae7f17943800155c3b001147
wb@stylite.de <wb@stylite.de>
2020-09-19 14:53:16 +0000

    File generated by Skaffolder - user: wb@stylite.de

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+// DEPENDENCIES
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// SECURITY
+import { AuthGuard } from './security/auth.guard';
+
+/**
+ * WEB APP ROUTES
+ */
+const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+    /* START MY VIEWS */
+
+    { path: 'home', loadChildren: './pages/home/home.module#HomeModule', canActivate: [AuthGuard] },
+
+ /* END MY VIEWS */
+
+    // SECURITY
+    { path: 'login', loadChildren: './pages/login/login.module#LoginModule' }
+];
+
+/**
+ * ROUTING MODULE
+ */
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
---
+++ src/app/core.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+
+/* START MY SERVICES IMPORTS */
+// Do not edit this comment content, it will be overwritten in next Skaffolder generation
+import { UserService } from './services/user.service';
+
+/* END MY SERVICES IMPORTS */
+
+import { AuthGuard } from './security/auth.guard';
+import { AuthenticationService } from './security/authentication.service';
+
+@NgModule({
+  imports: [],
+  providers: [
+    /* START PROVIDERS */
+// Do not edit this comment content, it will be overwritten in next Skaffolder generation
+    UserService,
+ /* END PROVIDERS */
+
+    // SECURITY
+    AuthGuard,
+    AuthenticationService,
+  ],
+  exports: []
+})
+export class CoreModule {
+}
